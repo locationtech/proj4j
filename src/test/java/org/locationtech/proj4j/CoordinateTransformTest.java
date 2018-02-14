897778ce734dc2e62b431388a981a3ba40422005
Will Cohen <wcohen@supernormal.io>
2018-02-14 17:39:34 -0500

    Change tolerance for Amersfoort_RD_New test, same as geotrellis
    Signed-off-by: Will Cohen <wcohen@supernormal.io>

--- src/test/java/org/locationtech/proj4j/CoordinateTransformTest.java
+++ src/test/java/org/locationtech/proj4j/CoordinateTransformTest.java
@@ -65,7 +65,7 @@ public class CoordinateTransformTest extends BaseCoordinateTransformTest {
      */
     @Test
     public void testAmersfoort_RD_New() {
-        checkTransformFromWGS84("EPSG:28992", 5.387638889, 52.156160556, 155029.79409195564, 463109.95436430885);
+        checkTransformFromWGS84("EPSG:28992", 5.387638889, 52.156160556, 155029.79409195564, 463109.95436430885, 2.0e-4);
     }
 
     @Test
