b85177988d8937236394413c7f88d749334640b6
Brian Osborn <bosborn@gmail.com>
2021-10-19 09:20:08 -0600

    newly fixed tests

--- src/test/java/org/locationtech/proj4j/CoordinateTransformTest.java
+++ src/test/java/org/locationtech/proj4j/CoordinateTransformTest.java
@@ -42,8 +42,8 @@ public class CoordinateTransformTest extends BaseCoordinateTransformTest {
   */
     }
 
-//    @Test
-    public void FAIL_testEPSG_27700() {
+    @Test
+    public void testEPSG_27700() {
         checkTransform("EPSG:4326", -2.89, 55.4, "EPSG:27700", 343733.1404, 612144.530677, 0.1);
         checkTransformAndInverse(
                 "EPSG:4326", -2.0301713578021983, 53.35168607080468,
